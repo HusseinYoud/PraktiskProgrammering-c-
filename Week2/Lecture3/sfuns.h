76c721dd1e18796cbe37622fd33d114ede119993
HusseinYoud <Husseinnaemalzaidi@gmail.com>
2026-02-16 09:21:58 +0100

    added week2

---
+++ Week2/Lecture3/sfuns.h
@@ -0,0 +1,9 @@
+// "sfuns.h" header file
+#ifndef HAVE_SFUNS_H
+#define HAVE_SFUNS_H
+namespace sfuns{
+    double fgamma(double x);
+    double sgamma(double x);
+}
+#endif // HAVE_SFUNS_H
+//ig det gør ingenting om tabbet eller ej. 
